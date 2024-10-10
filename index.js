3c0f0253acbc72ae5308c349afe15dba576e6ad6
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2024-10-10 12:29:26 -0400

    getrgersgetrs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const DOMSelectors = {
-    
+  form: document.querySelector("form"),
+  backgroundColor: document.querySelector("#color"),
+  imageLink: document.querySelector("#image"),
+  boxTitle: document.querySelector("#boxTitle"),
 };
 
-function 
+function createBox(color) {}
