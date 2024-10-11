a88a757367c08aa9b92a1ea705820a9dd4094a2d
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2024-10-11 12:25:13 -0400

    ff

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const DOMSelectors = {
   backgroundColor: document.querySelector("#color"),
   imageLink: document.querySelector("#image"),
   boxTitle: document.querySelector("#boxTitle"),
+  boxCaption: document.querySelector("#boxCaption"),
 };
 
-function createBox(color) {}
+function createBox() {
+  let bgclor = document.querySelector("color");
+  console.log *= bgclor;
+
+  bgcolor.addEventListener("input", function (event) {
+    event.preventDefault();
+    console.log(event, target);
+    background.style.backgroundColor = document.querySelector("color");
+  });
+}
