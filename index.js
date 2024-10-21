791ed3762ac020fb79a08edf8d6d17ccd9123930
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2024-10-20 23:32:27 -0400

    ea

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const DOMSelectors = {
   form: document.querySelector(".form"),
   boxTitleInput: document.querySelector("#boxTitle"),
   boxCaptionInput: document.querySelector("#boxCaption"),
-  boxColorInput: document.querySelector("#color"),
+  boxColorInput: document.querySelector("#bgcolor"),
+  boxTextColorInput: document.querySelector("#txtcolor"),
   imageInput: document.querySelector("#image"),
   submitButton: document.querySelector(".submit"),
   box: document.querySelector(".forms"),
 };
 
-function createBox(title, caption, bgColor, imageUrl) {
-  document.body.insertAdjacentHTML(
+function createBox(title, caption, bgColor, txtColor, imageUrl) {
+  const boxesContainer = document.querySelector(".boxes");
+
+  boxesContainer.insertAdjacentHTML(
     "beforeend",
     `
   <div class="box" style="background-color: ${bgColor};">
-    <h2>${title}</h2>
+    <h2 style="color: ${txtColor};">${title}</h2>
     ${imageUrl ? `<img src="${imageUrl}" alt="Uploaded Image" />` : ""}
-    <p>${caption}</p>
+    <p style="color: ${txtColor};">${caption}</p>
   </div>
   `
   );
@@ -23,13 +26,20 @@ function createBox(title, caption, bgColor, imageUrl) {
 
 function submitValues(event) {
   event.preventDefault();
+
+  if (!DOMSelectors.form.checkValidity()) {
+    DOMSelectors.form.reportValidity();
+    return;
+  }
+
   const title = DOMSelectors.boxTitleInput.value;
   const caption = DOMSelectors.boxCaptionInput.value;
   const bgColor = DOMSelectors.boxColorInput.value;
+  const txtColor = DOMSelectors.boxTextColorInput.value;
   const imageFile = DOMSelectors.imageInput.files[0];
   const imageUrl = imageFile ? URL.createObjectURL(imageFile) : "";
 
-  createBox(title, caption, bgColor, imageUrl);
+  createBox(title, caption, bgColor, txtColor, imageUrl);
   DOMSelectors.form.reset();
 }
 
